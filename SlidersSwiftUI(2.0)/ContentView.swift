cc552e6a45654b7f33ad4b5fa10172e58d934a36
Artem Sokolovskiy <artemsokol98@gmail.com>
2021-06-05 19:52:42 +0300

    Initial Commit

---
+++ SlidersSwiftUI(2.0)/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SlidersSwiftUI(2.0)
+//
+//  Created by Артем Соколовский on 05.06.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SlidersSwiftUI(2.0)/SlidersSwiftUI_2_0_App.swift
@@ -0,0 +1,17 @@
+//
+//  SlidersSwiftUI_2_0_App.swift
+//  SlidersSwiftUI(2.0)
+//
+//  Created by Артем Соколовский on 05.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct SlidersSwiftUI_2_0_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
